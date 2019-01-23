70ed5d9b7a1e70ab61290261285d3cebf591f814
dgstewart <ds98@wowway.com>
2019-01-23 17:43:22 -0500

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,47 +1,33 @@
-<?php
-session_start();
-if(isset($_SESSION["username"])){
-  if($_SESSION["expires_in"] < time()){
-    unset($_SESSION["username"]);
-    unset($_SESSION["expires_in"]);
-  }
-  else
-    header("Location: home");
-}
-?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Login</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.2/js/bootstrap.min.js"></script> 
 
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
-    <style type="text/css">
-body {
-    =======
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.2/js/bootstrap.min.js"></script> 
-
-    <style type="text/css">
+<style type="text/css">
+   
     body {
-        background-image: url("assets/background.jpg");
+        background: url(assets/background.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
         background-size: cover;
+        height: 100%;
+        overflow: hidden;
     }
-        .login-form {
-                width: 340px;
-        margin: 50px auto;
-        }
+    
+	.login-form {
+		width: 340px;
+    	margin: 50px auto;
+	}
     .login-form form {
-        margin-bottom: 15px;
+    	margin-bottom: 15px;
         background: #f7f7f7;
         box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
         padding: 30px;
@@ -65,36 +51,10 @@ body {
         width:auto;
     }
 
-    </style>
-    </head>
-
-    <body>
-    <div class="login-form">
-        <form action="login.php" method="post">
-            <img src="assets/logo4-final.png" class="img-fluid">
-
-            <h2 class="text-center">Log in</h2>
-
-            <div class="form-group">
-                <input type="text" name="username" class="form-control" placeholder="Username" required="required">
-            </div>
-
-            <div class="form-group">
-                <input type="password" name="password" class="form-control" placeholder="Password" required="required">
-            </div>
-
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Log in</button>
-            </div>
-
-            <div class="clearfix">
-                <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label> <a href="#" class="pull-right">Forgot Password?</a>
-            </div>
-        </form>
-
-        <p class="text-center"><a href="#">Create an Account</a></p>
-    </div>
-    <div class="login-form">
+</style>
+</head>
+<body>
+<div class="login-form">
     <form action="home.php" method="post">
         <img src="assets/logo4-final.png" class="img-fluid">
         <h2 class="text-center">Log in</h2>
@@ -114,15 +74,6 @@ body {
         <br>
         <button type="button" class="btn btn-info btn-block">Create an Account</button>
     </form>
-    </div>
-    </body>
-    </html>
-    </style>
-</head>
-
-<body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript">
-</script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript">
-</script>
+</div>
 </body>
-</html>
+</html>                                		                            
