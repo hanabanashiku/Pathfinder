3a2288209c948b7d52f2730ff074e49c7a4c8821
Michael MacLean <elemaster96@aol.com>
2019-02-04 12:38:03 -0500

    Favicon

--- website/index.php
+++ website/index.php
@@ -18,6 +18,7 @@ if(isset($_SESSION["username"]) || isset($_SESSION["expires_in"])){
 
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
+    <link rel="shortcut icon" href="assets/favicon.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript">
 </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript">
@@ -53,7 +54,7 @@ if(isset($_SESSION["username"]) || isset($_SESSION["expires_in"])){
         min-height: 38px;
         border-radius: 2px;
     }
-    .btn {        
+    .btn {
         font-size: 15px;
         font-weight: bold;
     }
