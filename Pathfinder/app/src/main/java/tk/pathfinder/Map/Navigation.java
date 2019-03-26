a9b91a196a47b8137703cd6af8237e35d0640ecc
Zack Steffes <zfsteffes@oakland.edu>
2019-03-26 14:11:09 -0400

    Merge branch 'AppUI' of /home/lzl/Documents/Pathfinder with conflicts.

--- Pathfinder/app/src/main/java/tk/pathfinder/Map/Navigation.java
+++ Pathfinder/app/src/main/java/tk/pathfinder/Map/Navigation.java
@@ -17,7 +17,7 @@ public class Navigation {
         return NavigatePath(map, current, goal, FloorConnector.FloorConnectorTypes.ELEVATOR);
     }
 
-    /**
+       /**
      * Find the shortest path between two locations.
      * @param map The map to navigate.
      * @param current The closest node to the user's current position.
@@ -29,4 +29,13 @@ public class Navigation {
     public static Path NavigatePath(Map map, Node current, Room goal, FloorConnector.FloorConnectorTypes connector_preference) throws NoValidPathException {
         throw new UnsupportedOperationException();
     }
+
+    //converts signal strength (in Mhz) to distance (in meters)
+    public double strengthToDistance(int signalStrength, int frequency){
+
+
+        double exp = (27.55 - (20 * Math.log10(frequency)) + Math.abs(signalStrength)) / 20.0;
+        return Math.pow(10.0, exp);
+    }
+
 }
